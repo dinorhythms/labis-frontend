2a939461d6eea00f9f9cc73c0a3082fe109c7bea
Oladehinde Olanrewaju Kazeem <larrysnet2001@gmail.com>
2020-11-16 14:57:07 +0100

    ch(router): setup react router dom
    - install react router dom
    - integrate react router dom
    - dummy signin page
    
    [Fixes #4]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './store';
 import './index.css';
@@ -9,11 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+		<BrowserRouter>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
 					<App />
 				</PersistGate>
 			</Provider>
+		</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
